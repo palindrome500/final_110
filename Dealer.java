003417010f9cde617bf80f4936106a2148481860
palindrome500 <matt.pereira500@gmail.com>
2014-11-17 17:43:57 -0500

    Create Dealer.java
    Creates ArrayList of 52 cards and deals them out to a set number of players

---
+++ Dealer.java
@@ -0,0 +1,18 @@
+/*
+Matthew Pereira
+This class will act as the dealer in a card game
+It will be able to:
+Create an array of all 52 cards
+Shuffle the deck
+Hand out a set amount of cards to a set amount of player decks
+*/
+
+import java.util.ArrayList;//needed for ArrayList use
+
+public class Dealer
+{
+   //Instance variables
+   private ArrayList<Card> deck; //the dealer's deck
+
+   private int dealIndex; //the current deck the dealer is giving to a player
+}
