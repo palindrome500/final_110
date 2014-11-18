d55d43ccc80f99a40b4eb2f2c69dce1e6654b089
palindrome500 <matt.pereira500@gmail.com>
2014-11-18 16:08:41 -0500

    Update Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -8,27 +8,108 @@ Hand out a set amount of cards to a set amount of player decks
 */
 
 import java.util.ArrayList;//needed for ArrayList use
+import java.util.Random; //Needed for shuffling
 
 public class Dealer
-{
+{  
+   //CONSTANTS
+   public static final int MIN_RANK = 2, DECK_MAX = 51, DECK_MIN = 0;
+
    //Instance variables
    private ArrayList<Card> deck; //the dealer's deck - stores Card objects
-
    private int dealIndex; //the current deck the dealer is giving to a player
+   private Random rand; //Random number generator
    
    /**
    Constructor
    only constructor - creates deck of 52 cards
    */
+   public Dealer()
+   {
+      //Create array list
+      deck = new ArrayList<Card>();
+      // create Random object
+      rand = new Random();
+   
+      for (int suit = Card.SPADES; suit <= Card.DIAMONDS; suit++)
+      {
+         for(int rank = MIN_RANK; rank <= Card.ACE; rank++ )
+         {
+            Card newCard = new Card(suit, rank);
+            deck.add(newCard);
+            //System.out.println(newCard); //Debug
+         }
+      }
+      
+      dealIndex = DECK_MIN;
+   }
    
    /**
    shuffle()
-   randomely mixes up cards in deck and makes
+   randomely mixes up cards in dealer deck
+   restarts deal Index
    */
+   public void shuffle()
+   {
+      Card temp; //Stores one of the cards while they're swapped
+      
+      for(int index = 0; index <= DECK_MAX; index++)
+      {
+         int swapWith = rand.nextInt(DECK_MAX + 1); //get a random index from 0 through 51
+         
+         temp = deck.get(index); //save current index in temp
+         
+         deck.set(index, deck.get(swapWith)); //set current index to swapWith
+         
+         deck.set(swapWith, temp); //copy temp to swapWith
+      }
+      
+      resetDealIndex();
+   }
    
    /**
-   deal(int players)
-   creates and returns a deck queue and give it x cards 
-   where x is 52 divided by the number of players
+   deal(int cards)
+   creates and returns a card from the index indicated by dealIndex
    */
+   public Card deal()
+   {
+      //try
+      //{
+      //   if (dealIndex > DECK_MAX) //IF
+      //      throw new OutOfCardsException("NO MORE CARDS!");
+            
+      //else
+      return deck.get(dealIndex++);//return a card and count up
+      //}
+      
+      // catch(new IndexOutOfBoundsException oob)
+//       {
+//          System.out.println("ERROR: No more cards to deal./nReset dealIndex or shuffle.")
+//          Card errorCard = new Card(Card.SPADES, Card.ACE);
+//          return errorCard;
+//       }
+   }
+   
+   
+   /**
+   resetDealIndex()
+   */
+   public void resetDealIndex()
+   {
+      dealIndex = DECK_MIN;
+   }
+   
+   
+   /**
+   getDealIndex()
+   */
+   public int getDealIndex()
+   {
+      return dealIndex;
+   }
+   
+//    public static void main(String [] args)
+//    {
+//       Dealer dealer = new Dealer();
+//    }
 }
