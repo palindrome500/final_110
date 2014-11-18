5f06301003e9bf5120e6adfb2720d66ac921747c
palindrome500 <matt.pereira500@gmail.com>
2014-11-18 16:07:43 -0500

    Create Deck.java

---
+++ Deck.java
@@ -0,0 +1,31 @@
+/*
+Matthew Pereira
+This class acts as a deck for a player
+It's based on a reference based queue
+Needs to work with the Card and Dealer classes
+*/
+
+public class Deck
+{
+   //Instance variables
+   Card top, bottom; //Top and bottom cards of the deck
+   
+   int size; //number of cards in deck
+   
+   /**
+   Constructor
+   */
+   
+   /**
+   takeCard() (dequeue)takes card from top
+   returns card that was taken
+   */
+   
+   /**
+   returnCard() (enqueue) adds card to bottom of deck
+   */
+   
+   /**
+   getSize() returns the number of cards in the deck
+   */
+}
