bb87817a3f7ecd165768d331bc63f34160e71658
palindrome500 <matt.pereira500@gmail.com>
2014-11-23 11:38:30 -0500

    Create WarGame.java

---
+++ WarGame.java
@@ -0,0 +1,59 @@
+/*
+Matthew Pereira
+This class will work out the main logic for the game of War
+*/
+
+public class WarGame
+{
+   //CONSTANTS
+   int HELF_DECK = 26, FULL_DECK = 52;
+   
+   //Class variables
+   
+   public static void main(String [] args)
+   {
+      //MAIN varirables###########
+      boolean winner = false;
+      
+      //Make dealer deck
+      
+      //Shuffle dealer deck
+      
+      //Create player deck
+      
+      //Deal out half of dealer deck to each player
+      
+      //Create discard pile for each player
+      
+      //Create WAR pile for each player
+      
+      //Start main loop //While winner == false
+         
+         //If there are still cards to draw
+            //Each player draw a card
+            
+            //Determine winner of battle
+               //If not a tie
+                  //Give both cards to winner's discard pile
+                  
+               //Else a tie go into WAR mode
+                  //While there are only ties
+                  
+                     //place tieing cards in WAR piles
+                     
+                     //Draw new card for each player
+                     
+                     //If cards don't tie
+                        //find winner
+                        
+                        //give in-Play cards and all WAR pile cards to winner's discard deck
+                        
+                        //isTie = false
+                        
+                     //Else is tie, continue loop
+               //END WAR LOOP
+      //END MAIN GAME LOOP
+      
+      //Determine winner (the one with the larger discard pile)
+   }
+}
