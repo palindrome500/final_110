42efcd6b2367e14e6d3e139ff70cdf9a7dcead2e
palindrome500 <matt.pereira500@gmail.com>
2014-12-03 15:31:44 -0500

    Create WarDriver.java

---
+++ WarDriver.java
@@ -0,0 +1,12 @@
+/**
+Matthew Pereira
+This is the driver for the War Game
+*/
+
+public class WarDriver
+{
+   public static void main(String [] args)
+   {
+      WarGUI gui = new WarGUI();
+   }
+}
